c7ab9ef7bd28fda4f9cb695fb265ffaf1022772a
GI-HYUK KWON <7lhyuk@gmail.com>
2025-06-23 15:58:48 +0900

    Feat(auth): Add auth router

--- backend/src/config.py
+++ backend/src/config.py
@@ -6,27 +6,41 @@ class DatabaseSettings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
+    POSTGRES_PORT: str
     POSTGRES_HOST: str
-
-    POSTGRES_DSN: PostgresDsn | None = None
+    POSTGRES_DSN: PostgresDsn = None
 
     model_config = SettingsConfigDict(
         env_file="../envs/.env.database",
         env_file_encoding='utf-8'
     )
 
-    @field_validator("POSTGRES_DNS", mode="before")
+    @field_validator("POSTGRES_DSN", mode="before")
     @classmethod
     def db_dsn(cls, v, info):
         if v is not None:
             return v
 
-        values = info.data
-
+        psql_config = info.data
         return (
-            f"postgresql://{values['POSTGRES_USER']}:{values['POSTGRES_PASSWORD']}"
-            f"@{values['POSTGRES_HOST']}:{values['POSTGRES_PORT']}/{values['POSTGRES_NAME']}"
+            f"postgresql://{psql_config['POSTGRES_USER']}"
+            f":{psql_config['POSTGRES_PASSWORD']}"
+            f"@{psql_config['POSTGRES_HOST']}"
+            f":{psql_config['POSTGRES_PORT']}"
+            f"/{psql_config['POSTGRES_DB']}"
         )
 
 
+class JWTSettings(BaseSettings):
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+    model_config = SettingsConfigDict(
+        env_file="../envs/.env.jwt",
+        env_file_encoding='utf-8'
+    )
+
 database_settings = DatabaseSettings()
+print(database_settings.POSTGRES_DSN)
+jwt_settings = JWTSettings()
