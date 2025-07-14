4c295696e35996d8fb7408433a8ce8e57c781247
GI-HYUK KWON <7lhyuk@gmail.com>
2025-07-14 19:23:34 +0900

    Chore: Set architecture

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../styles/Navbar.module.css";
+
+
+interface NavbarProps {
+  toggleSidebar: () => void;
+}
+
+const Navbar: React.FC<NavbarProps> = ({toggleSidebar}) => {
+    return (
+        <nav className={styles.}>
+
+        </nav>
+    )
+}
